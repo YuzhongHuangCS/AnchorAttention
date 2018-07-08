f308a2d9fb2b325eb211fe35d275233b5e1a6fd4
Yuzhong Huang <yuzhongh@usc.edu>
2018-07-07 23:48:27 -0700

    reformat code

--- main.py
+++ main.py
@@ -11,14 +11,14 @@ from json_writer import JSONWriter
 from plot_writer import PlotWriter
 
 if __name__ == "__main__":
-	config = Config()
-	json_w = JSONWriter(config)
-	plot_w = PlotWriter(config)
-	predictor = RNNPredictor(config)
+    config = Config()
+    json_w = JSONWriter(config)
+    plot_w = PlotWriter(config)
+    predictor = RNNPredictor(config)
 
-	filename = sys.argv[1]
-	content = json.loads(open(filename).read())
+    filename = sys.argv[1]
+    content = json.loads(open(filename).read())
 
-	predictor.predict(content)
-	plot_w.write(predictor)
-	json_w.write(predictor)
+    predictor.predict(content)
+    plot_w.write(predictor)
+    json_w.write(predictor)
--- run_all.py
+++ run_all.py
@@ -1,18 +1,18 @@
 import os
 import sys
 
-prefix = 'data/'
+prefix = 'data_copy/'
 files = os.listdir(prefix)
 
 if len(sys.argv) > 2:
-	start = int(sys.argv[1])
-	step = int(sys.argv[2])
+    start = int(sys.argv[1])
+    step = int(sys.argv[2])
 else:
-	start = 0
-	step = 1
+    start = 0
+    step = 1
 
 for i in range(start, len(files), step):
-	name = files[i]
-	cmd = 'python regular.py data/{} | tee log/{}'.format(name, name.replace('.json', '.log'))
-	print(cmd)
-	os.system(cmd)
+    name = files[i]
+    cmd = 'python regular.py data_copy/{} > log/{} 2>&1'.format(name, name.replace('.json', '.log'))
+    print(cmd)
+    os.system(cmd)
