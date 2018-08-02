5a10382ec58371230c7e3a21a6ab263dd62dbb40
Yuzhong Huang <yuzhongh@usc.edu>
2018-08-02 12:52:28 -0700

    tweak parameters

--- config.py
+++ config.py
@@ -11,16 +11,17 @@ class Config(object):
 
         self.n_batch = 1
         self.n_max_epoch = 1000
+        self.n_max_epoch_total = 100
         self.n_neurons = 256
         self.n_predict_step = 10
         self.n_input_dim = 1
-        self.n_output_dim = 30
-        self.n_patience = 20
-        self.n_lr_decay = 2
+        self.n_dense_dim = 128
+        self.n_output_dim = 3
+        self.n_patience = 100
+        self.n_lr_decay = 5
         self.lr = 1e-3
-        self.lr_decay = 0.9
-        self.ratio_valid = 0.1
-        self.max_valid = 100
-        self.valid_loss_weight = 0.6
+        self.lr_decay = 0.95
+        self.max_valid = 10
+        self.valid_loss_weight = 0.5
         self.test_split = True
         self.mse_threshold = 0.2
--- rnn_predictor.py
+++ rnn_predictor.py
@@ -59,7 +59,6 @@ class RNNPredictor(object):
 
         n_predict_step = 10
         self.config.n_predict_step = n_predict_step
-        self.config.n_output_dim = 3
 
         if self.config.test_split:
             data = data_all[:-self.config.n_predict_step]
@@ -73,43 +72,31 @@ class RNNPredictor(object):
         x = data_scaled.reshape(-1, 1)
 
 
-        n_valid = max(min(int(len(x) * self.config.ratio_valid), self.config.max_valid), self.config.n_predict_step)
+        n_valid = min(self.config.n_predict_step, self.config.max_valid)
         n_train = len(x) - n_valid
         n_total = len(x)
 
-        print('Predict step: ', self.config.n_predict_step, self.config.n_output_dim)
+        print('Predict step: ', self.config.n_predict_step)
         print('n_valid:', n_valid, 'n_train:', n_train, 'n_total:', n_total)
-        #x_train, y_train = x[:n_train], y[:n_train]
-        #x_valid, y_valid = x[n_train:], y[n_train:]
 
         # build network
         batchX_placeholder = tf.placeholder(tf.float32, [n_total, self.config.n_input_dim])
-        #batchY_placeholder = tf.placeholder(tf.float32, [n_total, 1])
 
-        W1 = tf.get_variable('W1', shape=(self.config.n_neurons, self.config.n_output_dim))
-        b1 = tf.get_variable('b1', shape=(1, self.config.n_output_dim), initializer=tf.zeros_initializer())
-        #W2 = tf.get_variable('W2', shape=(self.config.n_dense1_dim, self.config.n_output_dim))
-        #b2 = tf.get_variable('b2', shape=(1, self.config.n_output_dim), initializer=tf.zeros_initializer())
-        #W3 = tf.get_variable('W3', shape=(self.config.n_dense2_dim, self.config.n_output_dim))
-        #b3 = tf.get_variable('b3', shape=(1, self.config.n_output_dim), initializer=tf.zeros_initializer())
-        #W4 = tf.get_variable('W4', shape=(self.config.n_dense3_dim, self.config.n_output_dim))
-        #b4 = tf.get_variable('b4', shape=(1, self.config.n_output_dim), initializer=tf.zeros_initializer())
+        W1 = tf.get_variable('W1', shape=(self.config.n_neurons, self.config.n_dense_dim))
+        b1 = tf.get_variable('b1', shape=(1, self.config.n_dense_dim), initializer=tf.zeros_initializer())
+        W2 = tf.get_variable('W2', shape=(self.config.n_dense_dim, self.config.n_output_dim))
+        b2 = tf.get_variable('b2', shape=(1, self.config.n_output_dim), initializer=tf.zeros_initializer())
 
         # Unpack columns
         inputs_series = tf.reshape(batchX_placeholder, (1, -1, 1))
 
         # Forward passes
         cell = tf.nn.rnn_cell.GRUCell(self.config.n_neurons)
-        #cell2 = tf.nn.rnn_cell.GRUCell(self.config.n_neurons2)
-        #cell = tf.nn.rnn_cell.MultiRNNCell([cell1, cell2])
         cell_state = cell.zero_state(self.config.n_batch, dtype=tf.float32)
 
         states_series, current_state = tf.nn.dynamic_rnn(cell, inputs_series, initial_state=cell_state, parallel_iterations=1)
 
-        prediction = tf.matmul(tf.squeeze(states_series), W1) + b1
-        #prediction = tf.matmul(tf.tanh(tf.matmul(tf.squeeze(states_series), W1) + b1), W2) + b2
-        #prediction = tf.matmul(tf.tanh(tf.matmul(tf.tanh(tf.matmul(tf.squeeze(states_series), W1) + b1), W2) + b2), W3) + b3
-        #prediction = tf.matmul(tf.tanh(tf.matmul(tf.tanh(tf.matmul(tf.tanh(tf.matmul(tf.squeeze(states_series), W1) + b1), W2) + b2), W3) + b3), W4) + b4
+        prediction = tf.matmul(tf.tanh(tf.matmul(tf.squeeze(states_series), W1) + b1), W2) + b2
 
         pred_point_train = tf.slice(prediction, (0, 0), (n_train-n_predict_step, 1))
         pred_lower_train = tf.slice(prediction, (0, 1), (n_train-n_predict_step, 1))
@@ -119,12 +106,17 @@ class RNNPredictor(object):
         pred_lower_valid = tf.slice(prediction, (n_train-n_predict_step, 1), (n_valid, 1))
         pred_upper_valid = tf.slice(prediction, (n_train-n_predict_step, 2), (n_valid, 1))
 
-        pred_point_test = tf.slice(prediction, (n_train+n_valid-n_predict_step, 0), (n_predict_step, 1))
-        pred_lower_test = tf.slice(prediction, (n_train+n_valid-n_predict_step, 1), (n_predict_step, 1))
-        pred_upper_test = tf.slice(prediction, (n_train+n_valid-n_predict_step, 2), (n_predict_step, 1))
+        pred_point_test = tf.slice(prediction, (n_total-n_predict_step, 0), (n_predict_step, 1))
+        pred_lower_test = tf.slice(prediction, (n_total-n_predict_step, 1), (n_predict_step, 1))
+        pred_upper_test = tf.slice(prediction, (n_total-n_predict_step, 2), (n_predict_step, 1))
+
+        pred_point_total = tf.slice(prediction, (0, 0), (n_total-n_predict_step, 1))
+        pred_lower_total = tf.slice(prediction, (0, 1), (n_total-n_predict_step, 1))
+        pred_upper_total = tf.slice(prediction, (0, 2), (n_total-n_predict_step, 1))
 
         labels_series_train = batchX_placeholder[n_predict_step:n_train, :]
         labels_series_valid = batchX_placeholder[n_train:, :]
+        labels_series_total = batchX_placeholder[n_predict_step:, :]
 
         # the total loss take all predictions into account
         def get_total_loss(pred_point, pred_lower, pred_upper, label):
@@ -133,18 +125,19 @@ class RNNPredictor(object):
             diff_lower = (pred_lower - label)
             diff_p_l = tf.reduce_mean(tf.square(tf.clip_by_value(diff_lower, 0, 1e10)))
             diff_n_l = tf.reduce_mean(tf.square(tf.clip_by_value(diff_lower, -1e10, 0)))
-            lower_loss = diff_p_l * 0.95 + diff_n_l * 0.05
+            lower_loss = diff_p_l * 0.99 + diff_n_l * 0.01
 
             diff_upper = (pred_upper - label)
             diff_p_u = tf.reduce_mean(tf.square(tf.clip_by_value(diff_upper, 0, 1e10)))
             diff_n_u = tf.reduce_mean(tf.square(tf.clip_by_value(diff_upper, -1e10, 0)))
-            upper_loss = diff_p_u * 0.05 + diff_n_u * 0.95
+            upper_loss = diff_p_u * 0.01 + diff_n_u * 0.99
 
-            total_loss = point_loss + lower_loss + upper_loss
+            total_loss = point_loss + 0.5 * (lower_loss + upper_loss)
             return total_loss
 
         total_loss_train = get_total_loss(pred_point_train, pred_lower_train, pred_upper_train, labels_series_train)
         total_loss_valid = get_total_loss(pred_point_valid, pred_lower_valid, pred_upper_valid, labels_series_valid)
+        total_loss_total = get_total_loss(pred_point_total, pred_lower_total, pred_upper_total, labels_series_total)
 
         learning_rate = tf.Variable(self.config.lr, trainable=False)
         learning_rate_decay_op = learning_rate.assign(learning_rate * self.config.lr_decay)
@@ -154,12 +147,18 @@ class RNNPredictor(object):
         gradients, _ = tf.clip_by_global_norm(gradients, 5.0)
         train_step = optimizer.apply_gradients(zip(gradients, variables))
 
-        saver = tf.train.Saver()
-        save_path = self.config.model_prefix + basename.replace('json', 'ckpt')
+        gradients_total, variables_total = zip(*optimizer.compute_gradients(total_loss_total))
+        gradients_total, _ = tf.clip_by_global_norm(gradients_total, 5.0)
+        train_step_total = optimizer.apply_gradients(zip(gradients_total, variables_total))
+
+        #saver = tf.train.Saver()
+        #save_path = self.config.model_prefix + basename.replace('json', 'ckpt')
+
         with tf.Session() as sess:
             sess.run(tf.global_variables_initializer())
 
             smallest_loss = float('inf')
+            smallest_train_loss = float('inf')
             wait = 0
 
             def _save_weight():
@@ -177,8 +176,6 @@ class RNNPredictor(object):
             for i in range(self.config.n_max_epoch):
                 print('Epoch: {}/{}'.format(i, self.config.n_max_epoch))
                 # train
-
-
                 train_loss, valid_loss, _train_step = sess.run(
                 [total_loss_train, total_loss_valid, train_step],
                     feed_dict={
@@ -192,6 +189,7 @@ class RNNPredictor(object):
                 if wait <= self.config.n_patience:
                     if sum_loss < smallest_loss:
                         smallest_loss = sum_loss
+                        smallest_train_loss = train_loss
                         _save_weight()
                         wait = 0
                         print('New smallest')
@@ -204,11 +202,35 @@ class RNNPredictor(object):
                 else:
                     break
 
-            print('In test')
-            print('Min loss', smallest_loss)
-            #test
             _load_weights()
-            saver.save(sess, save_path)
+            print('Training loss', smallest_train_loss)
+
+            _total_loss = sess.run(
+                [total_loss_total],
+                feed_dict={
+                    batchX_placeholder: x,
+                    cell_state: _current_cell_state,
+                }
+            )
+
+            for i in range(self.config.n_max_epoch_total):
+                if _total_loss < smallest_train_loss:
+                    break
+
+                print('Epoch_total: {}/{}'.format(i, self.config.n_max_epoch_total))
+                _total_loss, _train_step = sess.run(
+                    [total_loss_total, train_step_total],
+                    feed_dict={
+                        batchX_placeholder: x,
+                        cell_state: _current_cell_state,
+                    }
+                )
+                print("Epoch_total", i, "Total Loss", _total_loss)
+
+            #test
+            print('In test')
+            print('Min loss', _total_loss)
+            #saver.save(sess, save_path)
 
             pred_train, pred_train_lower, pred_train_upper, pred_valid, pred_valid_lower, pred_valid_upper, pred_test, pred_test_lower, pred_test_upper = sess.run(
             [pred_point_train, pred_lower_train, pred_upper_train, pred_point_valid, pred_lower_valid, pred_upper_valid, pred_point_test, pred_lower_test, pred_upper_test],
